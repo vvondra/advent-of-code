db03d5cb93be715f81446bf46202ca462de926d3
Vojtech Vondra <v.vondra@foodpanda.com>
2015-12-12 23:38:46 +0100

    Just count the extra representation

--- 8/8.sh
+++ 8/8.sh
@@ -2,21 +2,17 @@
 
 
 BYTES_WO_WS=$(echo "$(wc -c < input ) - $(wc -l < input)" | bc)
-CHAR_COUNT=0
-while IFS='' read -r LINE || [[ -n "$LINE" ]]; do
-	#echo $LINE
-	LINE=$(sed "s/\x\([0-9][0-9]\)/\0\1/" <<< $LINE)
-	echo $LINE
-	CMD="INTERPOL=\$'${LINE:1:$(expr ${#LINE} - 2)}'"
-	#echo $CMD
-	#echo $INTERPOL
-	eval $CMD
-    CHAR_COUNT=$((CHAR_COUNT+${#INTERPOL}))
-    #echo $CHAR_COUNT
-    #echo "---"
-done < input
+
+ESCAPE_COUNT=$(grep -o '\\x[a-z0-9][a-z0-9]' input | wc -l)
+SLASH_COUNT=$(grep -o '\\\\' input | wc -l)
+QUOTE_COUNT=$(grep -o '\\"' input | wc -l)
+LINE_COUNT=$(wc -l < input)
+
 
 echo "Bytes: $BYTES_WO_WS"
-echo "Chars: $CHAR_COUNT"
+echo "Escapes: $ESCAPE_COUNT"
+echo "Slashes: $SLASH_COUNT"
+echo "Quotes: $QUOTE_COUNT"
+echo "Lines: $LINE_COUNT"
 
-echo $(($BYTES_WO_WS - $CHAR_COUNT))
+echo $((3 * $ESCAPE_COUNT + $SLASH_COUNT + $QUOTE_COUNT + 2 * ($LINE_COUNT + 1)))
