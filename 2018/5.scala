3a4b515a885aa23070706dd720188ea560b6d234
Vojtech Vondra <vojtavondra@gmail.com>
2018-12-05 23:33:35 +0100

    Unify solutions for A and B for Day 15

--- 2018/5.scala
+++ 2018/5.scala
@@ -1,24 +1,41 @@
 var polymer = io.Source.stdin.mkString.trim
 
-var reduced = false
-do {
-  val radicals = polymer.sliding(2,1)
-    .zipWithIndex
-    // Careful, XOR has really weird preference in Scala, lower than ==
-    .filter { case (pair, _) => pair.length == 2 && pair.charAt(0).toUpper == pair.charAt(1).toUpper && (pair.charAt(0).isUpper ^ pair.charAt(1).isUpper) }
+val mostEfficient = (('a' until 'z').toList ++ List(' '))
+  .par
+  .map(char => {
+    var reduced = false
+    // Find solutions for B
+    var letterPolymer = polymer.replaceAll("(?i)" + char, "")
+    do {
+      // Find opposite polarity pairs, return start index in origianl string
+      val radicals = letterPolymer.sliding(2,1)
+        .zipWithIndex
+        // Careful, XOR has really weird preference in Scala, lower than ==
+        .filter { case (pair, _) => pair.length == 2 && pair.charAt(0).toUpper == pair.charAt(1).toUpper && (pair.charAt(0).isUpper ^ pair.charAt(1).isUpper) }
 
-  val oldLength = polymer.length
-  polymer = radicals.foldLeft((polymer, 0, 0)) {
-    case ((reducedPolymer, lastIndex, cuts), radical) =>
-      if (radical._2 > 1 && radical._2 < lastIndex + 2) {
-        (reducedPolymer, lastIndex, cuts)
-      } else {
-        (reducedPolymer.substring(0, radical._2 - cuts * 2) + reducedPolymer.substring(radical._2 + 2 - cuts * 2), radical._2, cuts + 1)
-      }
-  }._1
+      val oldLength = letterPolymer.length
 
-  reduced = polymer.length < oldLength
-} while (reduced)
+      letterPolymer = radicals.foldLeft((letterPolymer, 0, 0)) {
+        case ((reducedPolymer, lastIndex, cuts), radical) =>
+          // If a pair such as aAa occurs, the sliding window would cut too many characters at once
+          // Skip the second pair and wait for another pass
+          if (radical._2 > 1 && radical._2 < lastIndex + 2) {
+            (reducedPolymer, lastIndex, cuts)
+          } else {
+            // The substring index has to be adjusted by already cut pairs in the previous runs
+            (reducedPolymer.substring(0, radical._2 - cuts * 2) + reducedPolymer.substring(radical._2 + 2 - cuts * 2), radical._2, cuts + 1)
+          }
+      }._1
 
-println(polymer)
-println(polymer.length)
+      reduced = letterPolymer.length < oldLength
+    } while (reduced)
+
+    // Progress bar :)
+    println(char, letterPolymer.length)
+
+    (char, letterPolymer.length)
+  })
+  .minBy(_._2) // Solution for task A is under the tuple with space as the first element
+
+
+println(mostEfficient)
