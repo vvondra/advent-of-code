852355f26fadf8d4cbefd4cfd4b8fa70d27aa060
Vojtech Vondra <vojtavondra@gmail.com>
2019-12-14 12:51:48 +0100

    Refactor 12 for Part 2

--- 2019/12.ts
+++ 2019/12.ts
@@ -2,7 +2,7 @@ import * as fs from "fs";
 
 type Coord = { x: number, y: number, z: number };
 
-const stars: Coord[] = fs.readFileSync("12.input", "utf-8")
+const input: Coord[] = fs.readFileSync("12.input", "utf-8")
   .trim()
   .split("\n")
   .map(s => {
@@ -13,55 +13,61 @@ const stars: Coord[] = fs.readFileSync("12.input", "utf-8")
     }, {}) as Coord;
   });
 
-const velocities = stars.map(x => ({ x: 0, y: 0, z: 0 }));
+const simulate = function * (initial: Coord[]): Generator<[Coord[], Coord[]]> {
+  const stars = initial.map(x => ({ x: x.x, y: x.y, z: x.z }))
+  const velocities = stars.map(x => ({ x: 0, y: 0, z: 0 }));
 
-let last = 0;
-for (let i = 0; i < 10000000; i++) {
-  for (let star = 0; star < stars.length; star++) {
-    const changes = { x: 0, y: 0, z: 0 };
-    for (let other = 0; other < stars.length; other++) {
-      for (const coord in changes) {
-        if (stars[star][coord] > stars[other][coord]) {
-          changes[coord]--;
-        } else if (stars[star][coord] < stars[other][coord]) {
-          changes[coord]++;
+  while (true) {
+    for (let star = 0; star < stars.length; star++) {
+      const changes = { x: 0, y: 0, z: 0 };
+      for (let other = 0; other < stars.length; other++) {
+        for (const coord in changes) {
+          if (stars[star][coord] > stars[other][coord]) {
+            changes[coord]--;
+          } else if (stars[star][coord] < stars[other][coord]) {
+            changes[coord]++;
+          }
         }
       }
-    }
 
-    for (const coord in changes) {
-      velocities[star][coord] += changes[coord];
-    }
-  }
-
-  for (let star = 0; star < stars.length; star++) {
-    for (const coord in velocities[star]) {
-      stars[star][coord] += velocities[star][coord];
-    }
-  }
-
-  if (i === 999) {
-    let energy = 0;
-    for (let star = 0; star < stars.length; star++) {
-      let potential = 0;
-      let kinetic = 0;
-      for (const coord in stars[star]) {
-        potential += Math.abs(stars[star][coord]);
-        kinetic += Math.abs(velocities[star][coord]);
+      for (const coord in changes) {
+        velocities[star][coord] += changes[coord];
       }
-
-      energy += potential * kinetic;
     }
 
-    console.log(energy);
-  }
+    for (let star = 0; star < stars.length; star++) {
+      for (const coord in velocities[star]) {
+        stars[star][coord] += velocities[star][coord];
+      }
+    }
 
-  if (stars[0].y === 2) {
-    console.log(i, i - last);
-    last = i;
-  }
-
-  if (i % 100000 === 0) {
-    console.log(i);
+    yield [stars, velocities]
   }
 }
+
+const energy = (stars: Coord[], velocities: Coord[]) => {
+  let total = 0;
+  for (let star = 0; star < stars.length; star++) {
+    let potential = 0;
+    let kinetic = 0;
+    for (const coord in stars[star]) {
+      potential += Math.abs(stars[star][coord]);
+      kinetic += Math.abs(velocities[star][coord]);
+    }
+
+    total += potential * kinetic;
+  }
+
+  return total;
+}
+
+let simulation = simulate(input);
+let counter = 1000;
+let last: [Coord[], Coord[]];
+while (counter > 0) {
+  last = simulation.next().value
+  counter--
+}
+
+simulation.return
+console.log(energy(...last));
