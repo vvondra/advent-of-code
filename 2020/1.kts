5ba45cfb4a137551b3ffb0696edc39c5340c0e23
Vojtech Vondra <vojtech.vondra@productboard.com>
2020-12-02 09:41:48 +0100

    2020: First two days

---
+++ 2020/1.kts
@@ -0,0 +1,11 @@
+import java.io.File
+
+val expenses = File("1.input").readLines().map(String::toInt)
+
+for (e1 in expenses) for (e2 in expenses) {
+	if (e1 + e2 == 2020) println(e1 * e2)
+}
+
+for (e1 in expenses) for (e2 in expenses) for (e3 in expenses) {
+	if (e1 + e2 + e3 == 2020) println(e1 * e2 * e3)
+}
