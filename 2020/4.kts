2b016e5a3de215df6981a18d0597e564997048f7
Vojtech Vondra <vojtech.vondra@productboard.com>
2020-12-04 10:38:52 +0100

    2020: Day 4

--- 2020/4.kts
+++ 2020/4.kts
@@ -8,7 +8,6 @@ val input = File("4.input").readText()
 	.map { it.map { pair -> pair.split(":").let { p -> p[0] to p[1] } }.toMap() }
 
 
-val required = listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
 val validators = mapOf(
 	"byr" to { value: String -> value.toIntOrNull()?.let { it >= 1920 && it <= 2002 } ?: false },
 	"iyr" to { value: String -> value.toIntOrNull()?.let { it >= 2010 && it <= 2020 } ?: false },
@@ -25,11 +24,11 @@ val validators = mapOf(
 	"pid" to { value: String -> value.matches(Regex("^[0-9]{9}$"))},
 )
 
-val validFirst = input.count { it.keys.containsAll(required) }
+val validFirst = input.count { it.keys.containsAll(validators.keys) }
 println(validFirst)
 
 val validSecond = input
-	.filter { it.keys.containsAll(required) }
+	.filter { it.keys.containsAll(validators.keys) }
 	.count { it.all { (key, value) -> validators.get(key)?.invoke(value) ?: true } }
 
 println(validSecond)
