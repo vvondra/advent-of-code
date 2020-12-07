facbd6651377f68bd130637af18be901a6a25581
Vojtech Vondra <vojtech.vondra@productboard.com>
2020-12-07 13:24:53 +0100

    2020: Day 7 refactoring

--- 2020/7.kts
+++ 2020/7.kts
@@ -5,23 +5,22 @@ val bags = File("7.input").readLines()
   .map { it.split(" bags contain ") }
   .associate { it[0] to it[1] }
   .mapValues {
-      it.value
-        .split(",")
-        .map { it.trim() }
-        .map { it.replace(".", "").replace(" bags", "").replace(" bag", "") }
-        .map { it.split(" ", limit = 2) }
-        .associate { it[1] to it[0] }
-        .mapValues { it.value.replace("no", "0") }
-        .mapValues { it.value.toInt() }
+    it.value
+      .split(",")
+      .map { it.trim() }
+      .map { it.replace(".", "").replace(" bags", "").replace(" bag", "") }
+      .map { it.split(" ", limit = 2) }
+      .associate { it[1] to it[0] }
+      .mapValues { it.value.replace("no", "0") }
+      .mapValues { it.value.toInt() }
   }
 
 fun containsBag(desired: String, current: String): Boolean {
-  val bag = bags.get(current)
+  val bag = bags.get(current).orEmpty()
 
-  if (bag == null) return false
   if (bag.getOrDefault(desired, 0) > 0) return true
 
-  return bag.keys.any { color -> containsBag(desired, color) }
+  return bag.keys.any { containsBag(desired, it) }
 }
 
 fun countBags(color: String): Int {
