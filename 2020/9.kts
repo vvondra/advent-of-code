5ffb427a715f618556c4ad094dff54d05e9d226b
Vojtech Vondra <vojtavondra@gmail.com>
2020-12-09 19:33:52 +0100

    2020: Day 9

--- 2020/9.kts
+++ 2020/9.kts
@@ -1,23 +1,19 @@
 import java.io.File
+import kotlin.system.measureTimeMillis
 
 val nums = File("9.input").readLines().map(String::toLong)
-val preamble = 25
+val windowLength = 25
 
-val firstInvalid = nums.windowed(preamble + 1)
-  .map { window ->
-    val preamble = window.take(preamble)
-    val head = window.last()
-
-    Pair(
-      head,
-      preamble.withIndex().any { (i, first) -> preamble.drop(i).any { second -> first + second == head } }
-    )
-  }
-  .find { (_, valid) -> !valid }!!
-  .let { it.first }
+val firstInvalid = nums.windowed(windowLength + 1)
+  .find { window ->
+    val preamble = window.take(windowLength).toSet()
+    !preamble.any { i -> preamble.minus(i).contains(window.last() - i) }
+  }!!
+  .let { it.last() }
 
 println(firstInvalid)
 
+// Part two, solution A, recalculating sum starting from each index
 nums.indices
   .map { i -> nums.drop(i) }
   .map {
@@ -31,3 +27,19 @@ nums.indices
   .first()
   .let { (it.minOrNull() ?: 0) + (it.maxOrNull() ?: 0) }
   .let(::println)
+
+// Part two, solution B, sliding a time window until the sum is just right
+generateSequence(Triple(0, 0, 0L)) { (i, j, sum) ->
+  val diff = firstInvalid - sum
+  //println("${i} ${j} ${sum}")
+  when {
+    diff > 0L -> Triple(i, j + 1, sum + nums[j])
+    diff < 0L -> Triple(i + 1, j, sum - nums[i])
+    else -> null
+  }
+}
+  .last()
+  .let { (i, j, _) -> nums.subList(i, j) }
+  .let { (it.minOrNull() ?: 0) + (it.maxOrNull() ?: 0) }
+  .let(::println)
+
