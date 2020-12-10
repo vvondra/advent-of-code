134fd3d6d1d0c23d544eab54c41978d1bfce1c31
Vojtech Vondra <vojtavondra@gmail.com>
2020-12-10 09:14:26 +0100

    2020: Day 10, part 2 naive

--- 2020/10.kts
+++ 2020/10.kts
@@ -8,3 +8,14 @@ chain.windowed(2)
   .groupingBy { it }
   .eachCount()
   .let { println(it[1]!! * it[3]!!) }
+
+fun count(start: Int, tail: List<Int>): Long {
+  if (tail.isEmpty()) return 1
+  return (1..(minOf(3, tail.count()))).map { skip ->
+    val head = tail[skip - 1]
+    if (head - start > 3) 0
+    else count(head, tail.drop(skip))
+  }.fold(0, Long::plus)
+}
+
+count(chain.first(), chain.drop(1)).let(::println)
