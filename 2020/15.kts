4c79a840b9cb035d1e6d4c7acd3d51c7db8d87fa
Vojtech Vondra <vojtech.vondra@productboard.com>
2020-12-15 18:26:49 +0100

    2020: Day 15

--- 2020/15.kts
+++ 2020/15.kts
@@ -1,26 +1,22 @@
-val input = listOf(19,20,14,0,9,1)
+data class Step(val history: MutableMap<Int, Int>, val turn: Int, val prev: Int, val beforeThat: Int? = null)
 
-data class Step(val history: Map<Int, Int>, val turn: Int, val prev: Int, val firstTime: Boolean, val beforeThat: Int? = null)
+fun seq(start: List<Int>): Sequence<Step> {
+  val initial = Step(
+    start.withIndex().associate { it.value to it.index + 1 }.toMutableMap(),
+    start.size + 1,
+    start.last()
+  )
 
-val initial = Step(
-  input.withIndex().associate { it.value to it.index + 1 },
-  input.size + 1,
-  input.last(),
-  true
-)
+  return generateSequence(initial) { (history, turn, _, beforeThat)  ->
+    val spoken = if (beforeThat == null) 0 else turn - 1 - beforeThat
+    val spokenBefore = history[spoken]
+    history.put(spoken, turn)
 
-val seq = generateSequence(initial) { (history, turn, _, firstTime, beforeThat)  ->
-  if (turn % 10000 == 0) {
-    println(turn)
-  }
-  if (firstTime) {
-    Step(history.plus(0 to turn), turn + 1, 0, !history.containsKey(0), history[0])
-  } else {
-    val spoken = turn - 1 - beforeThat!!
-    Step(history.plus(spoken to turn), turn + 1, spoken, !history.containsKey(spoken), history[spoken])
+    Step(history, turn + 1, spoken, spokenBefore)
   }
 }
 
+val input = listOf(19,20,14,0,9,1)
 
-seq.find { it.turn == 2021 }!!.let { println(it.prev) }
-seq.find { it.turn == 30000000 }!!.let { println(it.prev) }
+seq(input).find { it.turn == 2021 }!!.let { println(it.prev) }
+seq(input).find { it.turn == 30000001 }!!.let { println(it.prev) }
