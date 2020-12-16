4e9cad4d2ddbf86080fc4239e441d6946dacc4e8
Vojtech Vondra <vojtavondra@gmail.com>
2020-12-16 18:38:45 +0100

    2020: Day 16

--- 2020/16.kts
+++ 2020/16.kts
@@ -2,13 +2,21 @@ import java.io.File
 
 val parts = File("16.input").readText().split("\n\n")
 
+data class Constraint(val name: String, val rules: List<IntRange>) {
+  fun matches(n: Int) = rules.any { n in it }
+}
+
 val constraints = parts[0].split("\n")
   .map {
     it.split(": ")
       .let {
-        it[0] to it[1].split(" or ").map { it.split("-").let { IntRange(it[0].toInt(), it[1].toInt()) } }
+        Constraint(
+          it[0],
+          it[1].split(" or ").map { it.split("-").let { IntRange(it[0].toInt(), it[1].toInt()) } }
+        )
       }
   }
+
 val ticket = parts[1].split("\n").last().split(",").map(String::toInt)
 val nearby = parts[2].split("\n")
   .drop(1)
@@ -16,9 +24,7 @@ val nearby = parts[2].split("\n")
   .map { it.split(",").map(String::toInt) }
 
 val totallyInvalid = nearby
-  .flatMap {
-    it.filter { value -> constraints.none { (_, ranges) -> ranges.any { it.contains(value) } } }
-  }
+  .flatMap { it.filter { value -> constraints.none { it.matches(value) } } }
   .sum()
   .let(::println)
 
@@ -35,28 +41,26 @@ fun findMappings(mappings: List<List<Int>>): List<Int> {
     )
   }
 
-  val valid = nearby.filter {
-    it.all { value -> constraints.any { (_, ranges) -> ranges.any { it.contains(value) } } }
-  }
+  val valid = nearby.filter { it.all { value -> constraints.any { it.matches(value) } } }
 
-  val filtered = mappings.withIndex()
+  val feasible = mappings.withIndex()
     .map { (fieldIdx, constraintCandidates) ->
       constraintCandidates.filter { constraintIdx ->
         valid
           .map { it[fieldIdx] }
-          .all { fieldValue -> constraints[constraintIdx].second.any { it.contains(fieldValue) } }
+          .all { constraints[constraintIdx].matches(it) }
       }
     }
 
-  return eliminateMappings(filtered).map { it.single() }
+  return eliminateMappings(feasible).map { it.single() }
 }
 
 val mapping = findMappings((0 until ticket.size).map { (0 until constraints.size).toList() })
 
-val errorRate = ticket
-  .withIndex()
+val errorRate = ticket.withIndex()
   .map { (field, value) ->
-    if (constraints[mapping[field]].first.startsWith("departure")) value.toLong() else null
+    if (constraints[mapping[field]].name.startsWith("departure")) value.toLong()
+    else null
   }
   .filterNotNull()
   .reduce(Long::times)
