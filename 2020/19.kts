2a55c1ed76f93db48505ae2b4e80de692315bc85
Vojtech Vondra <vojtavondra@gmail.com>
2020-12-19 22:05:21 +0100

    2020: Day 19 - refactoring

--- 2020/19.kts
+++ 2020/19.kts
@@ -1,38 +1,31 @@
 import java.io.File
 
-abstract class Node
-data class Character(val char: Char) : Node()
-data class Refs(val seq: List<Int>) : Node()
-data class Or(val refs: List<Node>) : Node()
-object End : Node()
+abstract class Rule
+data class Character(val char: Char) : Rule()
+data class Refs(val seq: List<Int>) : Rule()
+data class Or(val refs: List<Rule>) : Rule()
+object End : Rule()
 
 val (regexInputs, inputs) = File("19.input").readText()
   .split("\n\n")
-  .let {
-    it[0].split("\n") to it[1].split("\n").filterNot(String::isEmpty)
-  }
+  .let { it[0].split("\n") to it[1].split("\n").filterNot(String::isEmpty) }
 
-fun parseRules(r: List<String>) =
-  r.associate {
-    val parts = it.split(": ")
-    parts[0].toInt() to parts[1]
-  }
-    .mapValues { (key, value) ->
-      if (value.startsWith("\"")) {
-        Character(value[1])
-      } else {
-        value.split(" | ")
-          .map { Refs(it.split(" ").map { it.toInt() }) }
-          .let { if (key == 0) it.single().copy(seq = it.single().seq.plus(-1)) else Or(it.reversed()) }
-      }
+fun parseRules(r: List<String>): Map<Int, Rule> =
+  r.associate { it.split(": ").let { it[0].toInt() to it[1] } }
+  .mapValues { (key, value) ->
+    if (value.startsWith("\"")) {
+      Character(value[1])
+    } else {
+      value.split(" | ")
+        .map { Refs(it.split(" ").map { it.toInt() }) }
+        .let { if (key == 0) it.single().copy(seq = it.single().seq.plus(-1)) else Or(it) }
     }
-    .plus(-1 to End)
+  }
+  .plus(-1 to End)
 
-val regexs = parseRules(regexInputs)
-
-fun match(rules: Map<Int, Node>, string: String): Boolean {
+fun match(rules: Map<Int, Rule>, string: String): Boolean {
   val success = "Yeeeees!"
-  fun accept(regex: Node, string: List<String>): List<String> {
+  fun accept(regex: Rule, string: List<String>): List<String> {
     return when (regex) {
       is Character -> string.filter { it.firstOrNull() == regex.char }.map { it.drop(1) }
       is End -> if (string.contains("")) listOf(success) else emptyList()
@@ -51,6 +44,8 @@ fun match(rules: Map<Int, Node>, string: String): Boolean {
 
   return res.contains(success)
 }
+
+val regexs = parseRules(regexInputs)
 
 inputs.count { match(regexs, it) }.let(::println)
 
