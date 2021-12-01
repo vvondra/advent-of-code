92cc7f4aecd82e4e54e4682d72c992055f3d82f9
Vojtech Vondra <vojtech.vondra@productboard.com>
2021-12-01 08:37:40 +0100

    This is not java

--- 2021/01.kts
+++ 2021/01.kts
@@ -4,9 +4,9 @@ val depths = File("01.input").readLines().map(String::toInt)
 
 depths.windowed(2)
     .count { it[0] < it[1] }
-    .let(System.out::println)
+    .let(::println)
 
 depths.windowed(3)
     .windowed(2)
     .count { it[0].sum() < it[1].sum() }
-    .let(System.out::println)
+    .let(::println)
