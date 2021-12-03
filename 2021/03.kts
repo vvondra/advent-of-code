884263ccca68af2ae1879ecbea28e578a83f82b4
Vojtech Vondra <vojtech.vondra@productboard.com>
2021-12-03 11:47:15 +0100

    2021, day 3 refactoring

--- 2021/03.kts
+++ 2021/03.kts
@@ -4,25 +4,29 @@ val input = File("03.input")
     .readLines()
     .map { it.map { it.digitToInt() } }
 
+fun List<Int>.fromBitList(): Int = this.joinToString("").toInt(radix = 2)
+fun Int.flipBit(): Int = 1 - this
+
 fun mostCommon(nums: List<List<Int>>): List<Int> {
     fun frequencies(): Map<Int, Pair<Int, Int>> =
-        nums.fold(emptyMap<Int, Pair<Int, Int>>()) { counts, reading ->
+        nums.fold(emptyMap()) { counts, reading ->
             (counts.keys + (0 until reading.count()))
                 .associateWith {
                     Pair(
-                        (counts[it]?.first ?: 0) + (1 - reading[it]),
+                        (counts[it]?.first ?: 0) + reading[it].flipBit(),
                         (counts[it]?.second ?: 0) + reading[it]
                     )
                 }
         }
 
-    val freqs = frequencies()
-    return freqs.keys.sorted().map { if (freqs[it]!!.first > freqs[it]!!.second) 0 else 1 }
+    return frequencies().let { freqs ->
+        freqs.keys.sorted().map { if (freqs[it]!!.first > freqs[it]!!.second) 0 else 1 }
+    }
 }
 
 val inputMostCommon = mostCommon(input)
-val gamma = inputMostCommon.joinToString("").toInt(2)
-val epsilon = inputMostCommon.map { 1 - it }.joinToString("").toInt(2)
+val gamma = inputMostCommon.fromBitList()
+val epsilon = inputMostCommon.map { it.flipBit() }.fromBitList()
 
 println(gamma * epsilon)
 
@@ -32,11 +36,11 @@ fun determineRating(max: Boolean): Int {
             iteration
         } else {
             val iterationMostCommon = mostCommon(iteration)
-            val bit = if (max) iterationMostCommon[index] else 1 - iterationMostCommon[index]
+            val bit = if (max) iterationMostCommon[index] else iterationMostCommon[index].flipBit()
             filter(iteration.filter { it[index] == bit }, index + 1)
         }
 
-    return filter(input, 0).first().joinToString("").toInt(2)
+    return filter(input, 0).first().fromBitList()
 }
 
 println(determineRating(max = true) * determineRating(max = false))
