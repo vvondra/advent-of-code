c916eaf6398797dcf785c1fe29ef1fde996b40f8
Vojtech Vondra <vojtech.vondra@productboard.com>
2021-12-03 09:46:15 +0100

    2021, day 3

--- 2021/03.kts
+++ 2021/03.kts
@@ -1,44 +1,42 @@
 import java.io.File
 
-val nums = File("03.input")
+val input = File("03.input")
     .readLines()
     .map { it.map { it.digitToInt() } }
 
-val freqs = nums.fold(emptyMap<Int, Pair<Int, Int>>()) { counts, reading ->
-    (counts.keys + (0 until reading.count()))
-        .associateWith {
-            Pair(
-                (counts[it]?.first ?: 0) + (1 - reading[it]),
-                (counts[it]?.second ?: 0) + reading[it]
-            )
+fun mostCommon(nums: List<List<Int>>): List<Int> {
+    fun frequencies(): Map<Int, Pair<Int, Int>> =
+        nums.fold(emptyMap<Int, Pair<Int, Int>>()) { counts, reading ->
+            (counts.keys + (0 until reading.count()))
+                .associateWith {
+                    Pair(
+                        (counts[it]?.first ?: 0) + (1 - reading[it]),
+                        (counts[it]?.second ?: 0) + reading[it]
+                    )
+                }
         }
+
+    val freqs = frequencies()
+    return freqs.keys.sorted().map { if (freqs[it]!!.first > freqs[it]!!.second) 0 else 1 }
 }
 
-val mostCommon = freqs.keys.sorted().map { if (freqs[it]!!.first > freqs[it]!!.second) 0 else 1 }
-val gamma = mostCommon.joinToString("").toInt(2)
-val epsilon = mostCommon.map { 1 - it }.joinToString("").toInt(2)
+val inputMostCommon = mostCommon(input)
+val gamma = inputMostCommon.joinToString("").toInt(2)
+val epsilon = inputMostCommon.map { 1 - it }.joinToString("").toInt(2)
 
 println(gamma * epsilon)
 
 fun determineRating(max: Boolean): Int {
-    fun filter(iteration: List<List<Int>>, index: Int): List<List<Int>> =
+    tailrec fun filter(iteration: List<List<Int>>, index: Int): List<List<Int>> =
         if (iteration.size <= 1) {
             iteration
         } else {
-
-
-            val bit = if (max) mostCommon[index] else 1 - mostCommon[index]
-            println(iteration)
-            println(index)
-            println(bit)
-            println("===")
+            val iterationMostCommon = mostCommon(iteration)
+            val bit = if (max) iterationMostCommon[index] else 1 - iterationMostCommon[index]
             filter(iteration.filter { it[index] == bit }, index + 1)
         }
 
-    return filter(nums, 0).first().joinToString("").toInt(2)
+    return filter(input, 0).first().joinToString("").toInt(2)
 }
-
-println(determineRating(max = true))
-println(determineRating(max = false))
 
 println(determineRating(max = true) * determineRating(max = false))
