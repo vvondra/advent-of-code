a35a115e25415fd6177f97c6d9ca79e2903b0bc1
Vojtech Vondra <vojtech.vondra@productboard.com>
2021-12-10 09:43:32 +0100

    2021, day 10

--- 2021/10.kts
+++ 2021/10.kts
@@ -1,11 +1,8 @@
 import java.io.File
-import java.util.*
 
 val input = File("10.input").readLines()
 
 val braces = mapOf('}' to '{', ']' to '[', ')' to '(', '>' to '<')
-val reverseBraces = braces.entries.associate { (k, v) -> v to k }
-val incomplete = ' '
 
 sealed class Res
 data class Illegal(val char: Char) : Res() {
@@ -18,39 +15,39 @@ data class Illegal(val char: Char) : Res() {
     }
 }
 
-data class Incomplete(val stack: Stack<Char>) : Res() {
-    fun point(char: Char) = when(char) {
-        ')' -> 1
-        ']' -> 2
-        '}' -> 3
-        '>' -> 4
+data class Incomplete(val stack: List<Char>) : Res() {
+    fun point(char: Char) = when (char) {
+        '(' -> 1
+        '[' -> 2
+        '{' -> 3
+        '<' -> 4
         else -> 0
     }
-    fun score() = stack.mapNotNull { reverseBraces[it] }.fold(0) { acc, c -> 5 * acc + point(c) }
+
+    fun score(): Long = stack.fold(0L) { acc, c -> 5 * acc + point(c) }
 }
 
 fun parse(line: String): Res {
-    tailrec fun loop(tail: String, stack: Stack<Char>): Res =
+    tailrec fun loop(tail: String, stack: List<Char>): Res =
         when {
             tail.isEmpty() -> Incomplete(stack)
             setOf('(', '<', '[', '{').contains(tail.first()) -> {
-                stack.push(tail.first())
-                loop(tail.drop(1), stack)
+                loop(tail.drop(1), listOf(tail.first()) + stack)
             }
             setOf(')', '>', ']', '}').contains(tail.first()) -> {
-                val s = stack.pop()
+                val s = stack.first()
                 if (braces[tail.first()] != s) {
                     Illegal(tail.first())
                 } else {
-                    loop(tail.drop(1), stack)
+                    loop(tail.drop(1), stack.drop(1))
                 }
             }
             else -> throw Exception("nono")
         }
 
-    return loop(line, Stack())
+    return loop(line, emptyList())
 }
 
 val parsed = input.map { parse(it) }
 parsed.filterIsInstance<Illegal>().map { it.score() }.sum().let(::println)
-parsed.filterIsInstance<Incomplete>().map { it.score() }.maxOrNull()?.let(::println) // todo median
+parsed.filterIsInstance<Incomplete>().map { it.score() }.sortedDescending().let { println(it[it.size / 2]) }
