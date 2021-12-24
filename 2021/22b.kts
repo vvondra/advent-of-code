b0c4b9755f459bd640a78f88801e6c0979927f49
Vojtech Vondra <vojtech.vondra@productboard.com>
2021-12-24 15:02:58 +0100

    2021, day 22

--- 2021/22b.kts
+++ 2021/22b.kts
@@ -1,16 +1,11 @@
 import java.io.File
 
-data class Range(val start: Int, val endInclusive: Int) {
-    fun intersects(o: Range): Boolean = start <= o.endInclusive && o.start <= endInclusive
-    fun intersect(o: Range): Range = Range(maxOf(start, o.start), minOf(endInclusive, o.endInclusive))
-}
-data class Ins(val on: Boolean, val x: Range, val y: Range, val z: Range) {
-    fun intersects(o: Ins) = x.intersects(o.x) && y.intersects(o.y) && z.intersects(o.z)
+data class Ins(val on: Boolean, val x: IntRange, val y: IntRange, val z: IntRange) {
+    fun intersects(o: Ins) = x.intersect(o.x).isNotEmpty() && y.intersect(o.y).isNotEmpty() && z.intersect(o.z).isNotEmpty()
     fun count() = (x.endInclusive - x.start + 1) *
             (y.endInclusive - y.start + 1) *
             (z.endInclusive - z.start + 1)
 }
-data class XYZ(val x: Int, val y: Int, val z: Int)
 
 val ins = File("22.input").readLines()
     .map { line ->
@@ -19,24 +14,60 @@ val ins = File("22.input").readLines()
 
         lineParts[1].split(",")
             .map { coords ->
-                coords.drop(2).split("..").let { Range(it[0].toInt(), it[1].toInt()) }
+                coords.drop(2).split("..").let { IntRange(it[0].toInt(), it[1].toInt()) }
             }.let { Ins(on, it[0], it[1], it[2]) }
     }
 
+fun mapCells(axis: (Ins) -> IntRange) =
+    ins
+        .flatMap { axis(it).let { a -> listOf(a.start, a.endInclusive + 1) } }
+        .sorted()
+        .distinct()
+        .withIndex()
 
-val total = ins.fold(0L to emptySet<Ins>()) { (count, counted), next ->
-    if (counted.none { it.intersects(next) }) {
-        println("new!==")
-        count + next.count() to counted + next
-    } else {
-        println("old!")
-        count to counted
+val xIndexMap = mapCells { it.x }.associate { (index, i) -> index to i }
+val xMap = mapCells { it.x }.associate { it.value to it.index }
+val yIndexMap = mapCells { it.y }.associate { (index, i) -> index to i }
+val yMap = mapCells { it.y }.associate { it.value to it.index }
+val zIndexMap = mapCells { it.z }.associate { (index, i) -> index to i }
+val zMap = mapCells { it.z }.associate { it.value to it.index }
+
+val mappedIns = ins.map {
+    Ins(
+        it.on,
+        xMap[it.x.start]!!..(xMap[it.x.endInclusive + 1]!! - 1),
+        yMap[it.y.start]!!..(yMap[it.y.endInclusive + 1]!! - 1),
+        zMap[it.z.start]!!..(zMap[it.z.endInclusive + 1]!! - 1),
+    )
+}
+
+val grid = Array(xIndexMap.size) { Array(yIndexMap.size) { Array(zIndexMap.size) { false } } }
+
+mappedIns.forEach { next ->
+    for (i in next.x) {
+        for (j in next.y) {
+            for (k in next.z) {
+                if (next.on) {
+                    grid[i][j][k] = true
+                } else {
+                    grid[i][j][k] = false
+                }
+            }
+        }
     }
 }
 
-val minZ = ins.minOf { it.z.start }
-val maxZ = ins.maxOf { it.z.endInclusive }
+var volume = 0L
+for (i in 0 until xIndexMap.size) {
+    for (j in 0 until yIndexMap.size) {
+        for (k in 0 until zIndexMap.size) {
+            if (grid[i][j][k]) {
+                volume += (xIndexMap[i + 1]!! - xIndexMap[i]!!).toLong() *
+                        (yIndexMap[j + 1]!! - yIndexMap[j]!!).toLong() *
+                        (zIndexMap[k + 1]!! - zIndexMap[k]!!).toLong()
+            }
+        }
+    }
+}
 
-println(minZ to maxZ)
-
-println(total)
+println(volume)
