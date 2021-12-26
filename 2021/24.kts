c8d217b2011c042c9dc1856cb98806c931411870
Vojtech Vondra <vojtech.vondra@productboard.com>
2021-12-26 19:39:03 +0100

    2021, day 24

--- 2021/24.kts
+++ 2021/24.kts
@@ -1,48 +1,98 @@
-
-sealed class Address {
-    class Const(val number: Int) : Address()
-    class Register(val add: Char) : Address()
-}
+import java.io.File
 
 
-data class Memory(val mem: Map<Char, Int>) {
-    fun get(a: Address): Int = when(a) {
-        is Address.Const -> a.number
-        is Address.Register -> mem.getValue(a.add)
+data class Op(val pop: Boolean, val a: Int, val b: Int)
+val params = File("24.input").readLines()
+    .chunked(18)
+
+val ops = params.map {
+        Op(
+            it[4].endsWith('6'),
+            it[5].split(' ').last().toInt(),
+            it[15].split(' ').last().toInt(),
+        )
     }
 
-    fun set(a: Address.Register, n: Int) = copy(mem + mapOf(a.add to n))
 
-}
+ops.forEach(::println)
+/*
+(0..17).forEach { row ->
+    print("${row.toString().padEnd(2, ' ')} ")
+    params.forEach {
+        print(it[row].padEnd(9, ' '))
+    }
+    println()
+}*/
 
-fun cycle(input: Int): Int {
-    var w: Int = input
-    var x: Int = 0
-    var y: Int = 0
-    var z: Int = 0
+fun cycle2(input: Int, zIn: Int, op: Op): Int {
+    println("Input: ${input}, Op: ${op}")
+    var w = input
+    var z = zIn
 
-    x = x * 0
-    x = z % 26
-    z = z / 1
-    x = x + 14
-    x = if (x == w) 1 else 0
-    x = if (x == 0) 1 else 0 // x = !x
-    y = 0
-    y = 25
-    y = 25 * x // y = 25x
-    y = y + 1 // y = 25x + 1
-    z = z * y // z = z (25x + 1)
-    y = y * 0 // y = 0
-    y = w + y // y = w
-    y = y + 1 // y = w + 1
-    y = x * y // y = x (w + 1)
-    z = z + y
+    val head = z % 26
 
-    println(Triple(x, y, z))
+    z = z / if (op.pop) 26 else 1
+
+    if (w != head + op.a) {
+        z *= 26
+        z += w + op.b
+    }
 
     return z
 }
 
-(1..9).forEach {
-    println(it to cycle(it))
+fun stack(stack: Int) {
+    var rem = stack
+    print("[ ${rem % 26}")
+    while (rem > 26) {
+        rem = rem / 26
+        print(" ${rem % 26} ")
+    }
+    println(" ]")
 }
+
+val inputs = List(14) { index ->
+    if (!ops[index].pop) {
+        var level = 1
+        var next = index
+        var instruction = ops[index]
+        while (level != 0 && index < 13) {
+            next = next + 1
+            instruction = ops[next]
+            if (instruction.pop) {
+                level--
+            } else {
+                level++
+            }
+        }
+
+        println("Matching for ${index} is ${instruction}")
+        (1..9).asSequence().first { f ->
+            ops[index].b + instruction.a + f in 1..9
+        }
+    } else 1
+    /*
+    part1:
+    (9 downTo 1).asSequence().first { f ->
+            ops[index].b + instruction.a + f in 1..9
+        }
+    } else 9
+
+     */
+}
+var z = 0
+val realInputs = mutableListOf<Int>()
+ops.withIndex().forEach { (i, op) ->
+    stack(z)
+    if (op.pop) {
+        val diff = (z % 26) + op.a
+        z = cycle2(diff, z, op)
+        realInputs.add(diff)
+    } else {
+        z = cycle2(inputs[i], z, op)
+        realInputs.add(inputs[i])
+    }
+}
+
+stack(z)
+println(realInputs.joinToString(""))
