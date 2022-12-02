c56bcc84a24d6b8da4e85ea189b422f3893ebf0f
Vojtech Vondra <vojtech.vondra@productboard.com>
2022-12-02 11:40:13 +0100

    day2

---
+++ 2022/02.kts
@@ -0,0 +1,26 @@
+import java.io.File
+
+val strategy = File("input/02.in")
+    .readLines()
+    .map { it.split(" ").let { it.first().single() - 'A' to it.last().single() - 'X' } }
+
+fun shapeScore(x: Int) = x + 1
+fun resultScore(x: Int, y: Int) = when {
+    x == y -> 3
+    x == (y + 1) % 3 -> 0
+    else -> 6
+}
+fun totalScore(game: List<Pair<Int, Int>>) = game
+    .fold(0) { score, (a, b) -> score + shapeScore(b) + resultScore(a, b) }
+
+totalScore(strategy).let(::println)
+
+val resolved = strategy.map { (a, b) ->
+    when (b) {
+        0 -> a to Math.floorMod(a - 1, 3)
+        1 -> a to a
+        2 -> a to Math.floorMod(a + 1, 3)
+        else -> throw Exception()
+    }
+}
+totalScore(resolved).let(::println)
