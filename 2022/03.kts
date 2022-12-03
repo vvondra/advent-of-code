756cc9bde98c51f86b09cf6c6d5020cc89cdabee
Vojtech Vondra <vojtech.vondra@productboard.com>
2022-12-03 08:17:11 +0100

    day3

---
+++ 2022/03.kts
@@ -0,0 +1,20 @@
+import java.io.File
+
+fun priority(c: Char) = when {
+    c.isLowerCase() -> c - 'a' + 1
+    c.isUpperCase() -> c - 'A' + 27
+    else -> throw Exception()
+}
+
+fun score(rucksacks: List<String>, chunker: (List<String>) -> List<List<String>>) = chunker(rucksacks)
+        .map { it.map(String::toSet).reduce(Set<Char>::intersect).single() }
+        .map(::priority)
+        .sum()
+
+val input = File("input/03.in").readLines()
+
+score(input) { r -> r.map { listOf(it.substring(0, it.length / 2), it.substring(it.length / 2)) } }
+    .let(::println)
+
+score(input) { it.chunked(3) }
+    .let(::println)
