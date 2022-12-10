53b050d36adebaa96267e30752f1eec093ebad27
Vojtech Vondra <vojtech.vondra@productboard.com>
2022-12-10 18:42:39 +0100

    day10

--- 2022/09.kts
+++ 2022/09.kts
@@ -6,7 +6,7 @@ val cmds = File("input/09.in")
     .readLines()
     .flatMap {
         val (dir, n) = it.split(" ")
-        List(n.toInt()) { _ -> dir.single() }
+        List(n.toInt()) { dir.single() }
     }
 
 data class XY(val x: Int, val y: Int) {
@@ -29,7 +29,7 @@ data class XY(val x: Int, val y: Int) {
 
 data class StepN(val knots: List<XY>, val visited: Set<XY> = setOf(knots.last()))
 fun visited(n: Int) = cmds
-    .fold(StepN(List(n) { _ -> XY(0, 0)})) { (knots, visited), cmd ->
+    .fold(StepN(List(n) { XY(0, 0)})) { (knots, visited), cmd ->
         val new = knots.drop(1).runningFold(knots.first().move(cmd)) { acc, xy -> xy.follow(acc) }
 
         StepN(new, visited + new.last())
---
+++ 2022/10.kts
@@ -0,0 +1,67 @@
+import java.io.File
+import java.util.*
+import kotlin.math.abs
+
+val cmds = File("input/10.in")
+    .readLines()
+    .map { it.split(" ") }
+    .map { when(it[0]) {
+        "noop" -> Op.Noop
+        "addx" -> Op.Addx(it[1].toInt())
+        else -> throw Exception("Unknown op ${it[0]}")
+    } }
+
+sealed class Op {
+    abstract val cycles: Int
+    abstract fun exec(registers: Map<Char, Int>): Map<Char, Int>
+
+    object Noop: Op() {
+        override val cycles: Int = 1
+        override fun exec(registers: Map<Char, Int>): Map<Char, Int> = registers
+    }
+
+    class Addx(val n: Int): Op() {
+        override val cycles: Int = 2
+        override fun exec(registers: Map<Char, Int>): Map<Char, Int> = registers + ('X' to registers['X']!! + n)
+    }
+}
+
+data class State(val pc: Int, val cycle: Int, val reg: Map<Char, Int>, val history: Map<Int, Int> = emptyMap())
+
+val breakpoints = (0..5).map { 20 + 40 * it }
+
+val state = generateSequence(State(0,1, mapOf('X' to 1), emptyMap())) { (pc, cycle, reg, history) ->
+    if (pc >= cmds.size) {
+        null
+    } else {
+        val cmd = cmds[pc]
+
+        val nextCycle = cycle + cmd.cycles
+        val nextValue = cmd.exec(reg)
+        val breakpoint = breakpoints.firstOrNull { it < cycle + cmd.cycles && !history.containsKey(it) }
+
+        State(pc + 1, nextCycle, nextValue, if (breakpoint != null) history + (breakpoint to reg['X']!!) else history)
+    }
+}.last()
+
+println(state.history.entries.fold(0) { acc, entry -> acc + entry.value * entry.key })
+
+generateSequence(State(0,1, mapOf('X' to 1), emptyMap())) { (pc, cycle, reg) ->
+    if (pc >= cmds.size) {
+        null
+    } else {
+        val cmd = cmds[pc]
+
+        val nextCycle = cycle + cmd.cycles
+        val nextValue = cmd.exec(reg)
+
+        (cycle until nextCycle).forEach { c ->
+            if (c % 40 in reg['X']!!..reg['X']!! + 2) print("█") else print(".")
+            if (c % 40 == 0) println()
+        }
+
+        State(pc + 1, nextCycle, nextValue)
+    }
+}.last()
+
+// FECZELHG
