7536290c85499d3a14707ea69574f7f87ffc37d3
Vojtech Vondra <vojtech.vondra@productboard.com>
2022-12-11 19:02:05 +0100

    refactor day11

--- 2022/11.kts
+++ 2022/11.kts
@@ -1,45 +1,37 @@
 import java.io.File
-import java.math.BigDecimal
-import java.math.RoundingMode
-import kotlin.math.roundToLong
 
 operator fun <T> List<T>.component6(): T = get(5)
-data class Monkey(val id: Int, val items: List<BigDecimal>, val op: (BigDecimal) -> BigDecimal, val div: BigDecimal, val yes: Int, val no: Int, val inspected: Int = 0) {
-    fun turn(worryDiv: BigDecimal): Pair<Monkey, Map<Int, List<BigDecimal>>> =
-            items.fold(emptyMap<Int, List<BigDecimal>>()) { throws, item ->
-                val worry = op(item).divide(worryDiv, RoundingMode.FLOOR)
-                val target = if (worry % div == BigDecimal.ZERO) yes else no
+data class Monkey(val id: Int, val items: List<Long>, val op: (Long) -> Long, val div: Long, val yes: Int, val no: Int, val inspected: Long = 0) {
+    fun turn(postOp: (Long) -> Long): Pair<Monkey, Map<Int, List<Long>>> =
+            items.fold(emptyMap<Int, List<Long>>()) { throws, item ->
+                val worry = postOp(op(item))
+                val target = if (worry % div == 0L) yes else no
                 throws + (target to throws.getOrDefault(target, emptyList()) + worry)
             }
             .let { throws ->
                 copy(items = emptyList(), inspected = inspected + throws.values.fold(0) { acc, b -> acc + b.size }) to throws
             }
 
-    fun receive(new: List<BigDecimal>) = copy(items = items + new)
+    fun receive(new: List<Long>) = copy(items = items + new)
 }
 
-val monkeyInputs = File("input/11.in").readText().split("\n\n")
-val divisor = monkeyInputs
-    .map { input ->
-        val (_, _, _, test) = input.split("\n")
-        test.split(" ").last().trim().toLong()
-    }
-    .reduce { a, b -> a * b }
-val monkeys = monkeyInputs
+val monkeys = File("input/11.in")
+    .readText()
+    .split("\n\n")
     .map { input ->
         val (m, s, op, test, yes, no) = input.split("\n")
-        val divTest = test.split(" ").last().trim().toBigDecimal()
+        val divTest = test.split(" ").last().trim().toLong()
 
         Monkey(
             Regex("\\d+").find(m)!!.value.toInt(),
-            s.split(":").last().trim().split(",").map { BigDecimal(it.trim()) },
+            s.split(":").last().trim().split(",").map { it.trim().toLong() },
             op.split("=").last().trim().let { exp ->
                 val (left, operand, right) = exp.split(" ")
-                fun(n: BigDecimal): BigDecimal {
-                    val a = if (left == "old") n else BigDecimal(left)
-                    val b = if (right == "old") n else BigDecimal(right)
+                fun(n: Long): Long {
+                    val a = if (left == "old") n else left.toLong()
+                    val b = if (right == "old") n else right.toLong()
                     return when (operand) {
-                        "*" -> (a * b).rem(BigDecimal(divisor))
+                        "*" -> a * b
                         "+" -> a + b
                         else -> throw Exception("Unexpected $operand")
                     }
@@ -51,16 +43,16 @@ val monkeys = monkeyInputs
         )
     }
 
-fun play(rounds: Int, worryDiv: BigDecimal) = generateSequence(monkeys.associateBy { it.id }) { round ->
+fun play(rounds: Int, postOp: (Long) -> Long) = generateSequence(monkeys.associateBy { it.id }) { round ->
     round.keys.sorted().fold(round) { turn, monkeyId ->
-        val (self, thrown) = turn[monkeyId]!!.turn(worryDiv)
+        val (self, thrown) = turn[monkeyId]!!.turn(postOp)
 
-        turn.mapValues { if (it.key in thrown) it.value.receive(thrown[it.key]!!) else it.value } + (monkeyId to self)
+        turn.mapValues { it.value.receive(thrown.getOrDefault(it.key, emptyList())) } + (monkeyId to self)
     }
 }
     .take(rounds + 1)
     .last()
-    .let { it.values.map { it.inspected }.sortedDescending().take(2).map(Int::toLong).reduce { a, b -> a * b} }
+    .let { it.values.map { it.inspected }.sortedDescending().take(2).reduce(Long::times) }
 
-println(play(20, BigDecimal(3)))
-println(play(10000, BigDecimal.ONE)) // 28407524722
+println(play(20) { it -> it / 3L })
+println(play(10000, { it -> it.rem(monkeys.map { it.div }.reduce(Long::times))}))
