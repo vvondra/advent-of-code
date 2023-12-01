af6f9d7c295767f6354d6c64e1443b7b50bc5b68
Vojtech Vondra <vvondra@microsoft.com>
2023-12-01 13:25:35 +0100

    2023, Day1, Part 2

--- 2023/Day1/Program.cs
+++ 2023/Day1/Program.cs
@@ -7,3 +7,31 @@ var result = input
     .Sum();
 
 Console.WriteLine(result);
+
+var replacements = new Dictionary<string, int> {
+    {"one", 1 },
+    {"two", 2 },
+    {"three", 3 },
+    {"four", 4 },
+    {"five", 5 },
+    {"six", 6 },
+    {"seven", 7 },
+    {"eight", 8 },
+    {"nine", 9}
+};
+var digits = replacements.Keys.Concat(Enumerable.Range(1, 9).Select(x => x.ToString()));
+
+var result2 = input
+    .Select(str =>
+    {
+        var min = digits.Select(digit => (digit, str.IndexOf(digit))).Where(x => x.Item2 > -1).MinBy(x => x.Item2);
+        var max = digits.Select(digit => (digit, str.LastIndexOf(digit))).Where(x => x.Item2 > -1).MaxBy(x => x.Item2);
+
+        return replacements.Aggregate($"{min.Item1}{max.Item1}", (acc, replacement)
+            => acc.Replace(replacement.Key, replacement.Value.ToString()));
+
+    })
+    .Select(int.Parse)
+    .Sum();
+
+Console.WriteLine(result2);
