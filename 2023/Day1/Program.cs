e3281677d100a24b42fd2d046f390df0a40a6987
Vojtech Vondra <vvondra@microsoft.com>
2023-12-01 10:17:15 +0100

    Day, part 1

---
+++ 2023/Day1/Program.cs
@@ -0,0 +1,9 @@
+﻿var input = Day1.Properties.Resources.input.Split(Environment.NewLine);
+
+var result = input
+    .Select(x => x.ToCharArray().Where(y => char.IsDigit(y)))
+    .Select(x => $"{x.First()}{x.Last()}")
+    .Select(int.Parse)
+    .Sum();
+
+Console.WriteLine(result);
