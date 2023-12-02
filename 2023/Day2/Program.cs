0beb224b7ac88327d70e2c9a6bb0e7d563437397
Vojtech Vondra <vojtech.vondra@productboard.com>
2023-12-02 11:35:57 +0100

    2023, day2

--- 2023/Day2/Program.cs
+++ 2023/Day2/Program.cs
@@ -1,26 +1,49 @@
 ﻿var input = Day2.Properties.Resources.input.Split(Environment.NewLine);
 
 var games = input
-    .Select(x =>
+    .Where(row => !string.IsNullOrWhiteSpace(row))
+    .Select(row =>
     {
-        var parts = x.Split(":");
+        var parts = row.Split(":");
         var id = int.Parse(parts[0].Split(" ").Last().Trim());
         var draws = parts[1].Split(";").Select(draw =>
-        {
-            draw.Split(", ").Select(ball =>
-            {
-                return ball.Split(" ")
-                    .Select(x => x.Trim())
-                    .ToList()
-                    .ToDictionary(
-                    y => y[1],
-                    y => int.Parse(y[0])
-            });
-        });
+            draw.Split(", ")
+                .Select(x => x.Trim().Split(" "))
+                .ToDictionary(
+                    x => x[1],
+                    x => int.Parse(x[0])
+                )
+        ).ToList();
 
         return new Game(id, draws);
     })
+    .ToList();
 
+var criteria = new Dictionary<string, int> {
+    {"red", 12 },
+    {"green", 13 },
+    {"blue", 14 }
+};
 
+var result = games
+    .Where(game => game.Draws.All(draw => draw.All(ball => criteria[ball.Key] >= ball.Value)))
+    .Select(game => game.Id)
+    .Sum();
 
-public record Game(int Id, List<Dictionary<string, int>> Draws);
+Console.WriteLine(result);
+
+var result2 = games
+    .Select(game => game.Draws.Aggregate(new Dictionary<string, int>(), (acc, draw) =>
+    {
+        foreach (var ball in draw)
+        {
+            acc[ball.Key] = int.Max(acc.GetValueOrDefault(ball.Key), ball.Value);
+        }
+        return acc;
+    }))
+    .Select(agg => agg.Aggregate(1, (acc, set) => acc * set.Value))
+    .Sum();
+
+    Console.WriteLine(result2);
+
+public record Game(int Id, IEnumerable<Dictionary<string, int>> Draws);
