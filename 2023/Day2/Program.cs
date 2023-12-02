34121960c50c91001cbf604e97c05841ab8a5afe
Vojtech Vondra <vvondra@microsoft.com>
2023-12-02 10:59:52 +0100

    wip

---
+++ 2023/Day2/Program.cs
@@ -0,0 +1,26 @@
+﻿var input = Day2.Properties.Resources.input.Split(Environment.NewLine);
+
+var games = input
+    .Select(x =>
+    {
+        var parts = x.Split(":");
+        var id = int.Parse(parts[0].Split(" ").Last().Trim());
+        var draws = parts[1].Split(";").Select(draw =>
+        {
+            draw.Split(", ").Select(ball =>
+            {
+                return ball.Split(" ")
+                    .Select(x => x.Trim())
+                    .ToList()
+                    .ToDictionary(
+                    y => y[1],
+                    y => int.Parse(y[0])
+            });
+        });
+
+        return new Game(id, draws);
+    })
+
+
+
+public record Game(int Id, List<Dictionary<string, int>> Draws);
