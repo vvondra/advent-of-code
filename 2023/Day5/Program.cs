48fb14d19e11f33b4a56616b369541e9f2f5acc1
Vojtech Vondra <vvondra@microsoft.com>
2023-12-05 20:03:51 +0100

    day5 wip

---
+++ 2023/Day5/Program.cs
@@ -0,0 +1,13 @@
+﻿var input = File.ReadAllText("input.txt").Split(Environment.NewLine + Environment.NewLine, StringSplitOptions.TrimEntries).ToArray();
+
+var seeds = input[0].Split(": ")[1].Split(" ", StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
+
+var maps = input[1..].Select(block => {
+    var split = block.Split(Environment.NewLine);
+    var mapping = split[0][..(split[0].Length - 5)].Split("-to-");
+    var mappings = split[1..].Select(line => line.Split(" ").Select(int.Parse).ToArray()).ToArray();
+
+    return (mapping: (from: mapping[0], to: mapping[1]), mappings);
+});
+
+maps.Select(x => x.ToString()).ToList().ForEach(Console.WriteLine);
