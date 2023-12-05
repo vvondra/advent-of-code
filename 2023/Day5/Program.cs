a52994e1118958bae36cb78bfd2ed85002a0c9da
Vojtech Vondra <vojtech.vondra@productboard.com>
2023-12-05 21:39:53 +0100

    2023, day5 part 1

--- 2023/Day5/Program.cs
+++ 2023/Day5/Program.cs
@@ -1,13 +1,36 @@
 ﻿var input = File.ReadAllText("input.txt").Split(Environment.NewLine + Environment.NewLine, StringSplitOptions.TrimEntries).ToArray();
 
-var seeds = input[0].Split(": ")[1].Split(" ", StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
+var seeds = input[0].Split(": ")[1].Split(" ", StringSplitOptions.TrimEntries).Select(long.Parse).ToArray();
 
-var maps = input[1..].Select(block => {
-    var split = block.Split(Environment.NewLine);
-    var mapping = split[0][..(split[0].Length - 5)].Split("-to-");
-    var mappings = split[1..].Select(line => line.Split(" ").Select(int.Parse).ToArray()).ToArray();
+var maps = input[1..]
+    .Select(block => {
+        var split = block.Split(Environment.NewLine);
+        var mapping = split[0][..(split[0].Length - 5)].Split("-to-");
+        var mappings = split[1..].Select(line => line.Split(" ").Select(long.Parse).ToArray()).ToArray();
 
-    return (mapping: (from: mapping[0], to: mapping[1]), mappings);
-});
+        return (mapping: (from: mapping[0], to: mapping[1]), mappings);
+    })
+    .ToDictionary(x => x.mapping.from, x => x);
 
-maps.Select(x => x.ToString()).ToList().ForEach(Console.WriteLine);
+
+
+var result = seeds
+    .Select(seed => {
+        var current = "seed";
+        var num = seed;
+
+        while (current != "location") {
+            var mapping = maps[current];
+
+            var range = mapping.mappings.FirstOrDefault(ranges => num >= ranges[1] && num < ranges[1] + ranges[2])
+                ?? [1, 1, 1];
+
+            num = num - range[1] + range[0];
+            current = mapping.mapping.to;
+        }
+
+        return num;
+    })
+    .Min();
+
+Console.WriteLine(result);
