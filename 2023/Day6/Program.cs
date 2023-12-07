1230b104101da19978bb709a9c5c2883b615d6af
Vojtech Vondra <vojtech.vondra@productboard.com>
2023-12-07 18:33:39 +0100

    2023, Day 6

---
+++ 2023/Day6/Program.cs
@@ -0,0 +1,32 @@
+﻿var input = File.ReadLines("input.txt")
+    .Select(line =>
+        line
+            .Split(":", StringSplitOptions.TrimEntries)[1]
+            .Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse).ToArray()
+    )
+    .ToArray();
+
+var times = input[0];
+var distances = input[1];
+
+var result = Enumerable.Range(0, times.Length)
+    .Select(i => Enumerable.Range(0, times[i]).Count(time => (times[i] - time) * time > distances[i]))
+    .Aggregate(1, (acc, val) => acc * val);
+
+Console.WriteLine(result);
+
+var input2 = File.ReadLines("input.txt")
+    .Select(line =>
+        line
+            .Split(":", StringSplitOptions.TrimEntries)[1]
+    )
+    .Select(num => num.Replace(" ", ""))
+    .ToArray();
+
+var time = int.Parse(input2[0]);
+var distance = long.Parse(input2[1]);
+
+var result2 = Enumerable.Range(0, time).Count(i => ((long)time - i) * i > distance);
+
+Console.WriteLine(result2);
