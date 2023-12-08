6f1795dacee58a53d9bd8d040278c7069aafa840
Vojtech Vondra <vojtech.vondra@productboard.com>
2023-12-08 18:53:44 +0100

    2023, Day 7

---
+++ 2023/Day7/Program.cs
@@ -0,0 +1,70 @@
+﻿var input = File.ReadLines("input.txt");
+
+var result = input
+    .Select(line => line.Split(" "))
+    .Select(parts => (hand: new PokerHand(parts[0]), bet: int.Parse(parts[1])))
+    .OrderBy(c => c.hand)
+    .Select((acc, idx) => (idx + 1) * acc.bet)
+    .Sum();
+
+Console.WriteLine(result);
+
+
+public record Card(char Value) : IComparable<Card>
+{
+    public int CompareTo(Card other)
+    {
+        const string order = "23456789TJQKA";
+        return order.IndexOf(Value).CompareTo(order.IndexOf(other.Value));
+    }
+
+    public override String ToString() => $"{Value}";
+}
+
+public record PokerHand(Card[] Cards) : IComparable<PokerHand>
+{
+    public PokerHand(String chars) : this(chars.Select(c => new Card(c)).ToArray()) { }
+
+    public int[] HighestPairs => Cards
+            .GroupBy(card => card.Value)
+            .ToDictionary(group => group.Key, group => group.Count()).Values.ToArray()
+            .OrderByDescending(c => c)
+            .ToArray();
+
+    public int CompareTo(PokerHand? other)
+    {
+        if (HighestPairs[0] != other.HighestPairs[0])
+        {
+            return HighestPairs[0] - other.HighestPairs[0];
+        }
+        else
+        {
+            if (HighestPairs[0] == 3 && other.HighestPairs[0] == 3)
+            {
+                if (HighestPairs[1] != other.HighestPairs[1])
+                {
+                    return HighestPairs[1] - other.HighestPairs[1];
+                }
+            }
+
+            if (HighestPairs[0] == 2 && other.HighestPairs[0] == 2) {
+                if (HighestPairs[1] != other.HighestPairs[1])
+                {
+                    return HighestPairs[1] - other.HighestPairs[1];
+                }
+            }
+
+            for (int i = 0; i < Cards.Length; i++)
+            {
+                if (Cards[i] != other.Cards[i])
+                {
+                    return Cards[i].CompareTo(other.Cards[i]);
+                }
+            }
+
+            return 0;
+        }
+    }
+
+    public override string ToString() => string.Join("", (IEnumerable<Card>)Cards);
+}
