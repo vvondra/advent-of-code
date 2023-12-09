90e5a1e4f918bd9dd43b4000bccdd3dd90a536fe
Vojtech Vondra <vojtech.vondra@productboard.com>
2023-12-09 22:35:57 +0100

    2023, Day 9, part 1

---
+++ 2023/Day9/Program.cs
@@ -0,0 +1,23 @@
+﻿var input = File.ReadLines("input.txt")
+    .Select(line => line.Split(" ", StringSplitOptions.TrimEntries).Select(int.Parse).ToArray())
+    .ToArray();
+
+
+IEnumerable<int> Differences(IEnumerable<int> seq) => seq.Zip(seq.Skip(1), (a, b) =>  b - a);
+bool AllZero(IEnumerable<int> seq) => seq.All(x => x == 0);
+IEnumerable<IEnumerable<int>> Reduce(IEnumerable<int> seq)
+{
+    var reduced = seq;
+    yield return seq;
+    while (!AllZero(reduced)) {
+        reduced = Differences(reduced);
+        yield return reduced;
+    }
+}
+
+var result = input
+    .Select(Reduce)
+    .SelectMany(x => x.Select(y => y.Last()))
+    .Sum();
+
+Console.WriteLine(result);
