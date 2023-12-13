4115a6c210f2f35892dcbd43572b6423a6c86b3e
Vojtech Vondra <vojtech.vondra@productboard.com>
2023-12-13 21:59:20 +0100

    2023, Day 12, Day13 WIP

---
+++ 2023/Day12/Program.cs
@@ -0,0 +1,65 @@
+﻿var input = File.ReadLines("input.txt").Select(line =>
+{
+    var parts = line.Split(" ");
+
+    return (record: parts[0], nums: parts[1].Split(",").Select(int.Parse).ToArray());
+}).ToList();
+
+static long Arrangements(string tail, int[] nums)
+{
+    long Arrangements2(string tail, int chomped, ArraySegment<int> nums)
+    {
+        if (nums.Sum() + nums.Count > tail.Length + chomped) {
+            return 0;
+        }
+
+        if (nums.Count == 0 && tail.Contains('#')) {
+            return 0;
+        }
+
+        if (nums.Count == 0 && !tail.Contains('#')) {
+            return 1;
+        }
+
+        return tail[0] switch
+        {
+            '?' => Arrangements2("#" +tail[1..], chomped, nums)
+                    + Arrangements2("." + tail[1..], chomped, nums),
+            '.' => chomped switch
+            {
+                0 => Arrangements2(tail[1..], 0, nums),
+                _ when chomped == nums[0] => Arrangements2(tail[1..], 0, nums[1..]),
+                _ => 0,
+            },
+            '#' => chomped switch
+            {
+                _ when nums.Count == 0 => 0,
+                _ when chomped <= nums[0] => Arrangements2(tail[1..], chomped + 1, nums),
+                _ => 0,
+            },
+            'X' => chomped switch
+            {
+                _ when nums.Count == 0 && chomped == 0 => 1,
+                _ when chomped == nums[0] && nums.Count == 1 => 1,
+                _ => 0,
+            },
+            _ => throw new NotImplementedException()
+        };
+    }
+    return Arrangements2(tail + 'X', 0, nums);
+}
+
+
+var result = input.Select(x => Arrangements(x.record, x.nums)).Sum();
+Console.WriteLine(result);
+
+var result2 = input
+    .Select(x => (record: string.Join('?', Enumerable.Repeat(x.record, 5)), nums: Enumerable.Repeat(x.nums, 5).SelectMany(x => x).ToArray()))
+    .AsParallel().WithDegreeOfParallelism(8).WithExecutionMode(ParallelExecutionMode.ForceParallelism)
+    .Select(x => (x, Arrangements(x.record, x.nums)))
+    .Select(x => {Console.WriteLine($"{x.Item2,20} {x.Item1.record}"); return x.Item2;})
+    .ToList();
+
+    //.Sum();
+
+Console.WriteLine(result2.Sum());
---
+++ 2023/Day13/Program.cs
@@ -0,0 +1,107 @@
+﻿var input = File.ReadAllText("input.txt").Split(Environment.NewLine + Environment.NewLine, StringSplitOptions.TrimEntries)
+                .Select(StringTo2DArray);
+
+
+var result = input.Select(SymmetryScore).Sum();
+
+Console.WriteLine(result);
+
+static char[,] StringTo2DArray(string input)
+{
+    var lines = input.Split('\n');
+    int rows = lines.Length;
+    int cols = lines[0].Length;
+
+    char[,] grid = new char[rows, cols];
+
+    foreach (var (line, rowIndex) in lines.Select((line, index) => (line, index)))
+    {
+        foreach (var (ch, colIndex) in line.Select((ch, index) => (ch, index)))
+        {
+            grid[rowIndex, colIndex] = ch;
+        }
+    }
+
+    return grid;
+}
+
+static int SymmetryScore(char[,] grid)
+{
+    int rows = grid.GetLength(0);
+    int cols = grid.GetLength(1);
+
+    var total = 0;
+
+    // Check rows for symmetry
+    for (int i = 0; i < rows - 1; i++)
+    {
+        if (GetRow(grid, i).SequenceEqual(GetRow(grid, i + 1)))
+        {
+            if (IsSymmetric(grid, i, true))
+            {
+                total += 100 * (i + 1);
+            }
+        }
+    }
+
+    // Check columns for symmetry
+    for (int j = 0; j < cols - 1; j++)
+    {
+        if (GetColumn(grid, j).SequenceEqual(GetColumn(grid, j + 1)))
+        {
+            if (IsSymmetric(grid, j, false))
+            {
+                total += j + 1;
+            }
+        }
+    }
+
+    return total;
+}
+static bool IsSymmetric(char[,] grid, int index, bool isRow)
+{
+    int rows = grid.GetLength(0);
+    int cols = grid.GetLength(1);
+    int length = isRow ? rows : cols;
+
+    // Start comparing from the rows or columns next to the center of symmetry
+    for (int offset = 1; index + offset < length && index - offset >= 0; offset++)
+    {
+        int firstIndex = index - offset;
+        int secondIndex = index + offset + 1; // Skip over the second row/column that co-defines the center
+
+        if (isRow)
+        {
+            if (firstIndex < 0 || secondIndex >= rows) break; // Bounds checking
+            if (!GetRow(grid, firstIndex).SequenceEqual(GetRow(grid, secondIndex)))
+                return false;
+        }
+        else
+        {
+            if (firstIndex < 0 || secondIndex >= cols) break; // Bounds checking
+            if (!GetColumn(grid, firstIndex).SequenceEqual(GetColumn(grid, secondIndex)))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+
+static IEnumerable<char> GetRow(char[,] grid, int rowIndex)
+{
+    int cols = grid.GetLength(1);
+    for (int j = 0; j < cols; j++)
+    {
+        yield return grid[rowIndex, j];
+    }
+}
+
+static IEnumerable<char> GetColumn(char[,] grid, int colIndex)
+{
+    int rows = grid.GetLength(0);
+    for (int i = 0; i < rows; i++)
+    {
+        yield return grid[i, colIndex];
+    }
+}
