72db42b2eb0a79aa12bae06db9319c5e89f48782
Vojtech Vondra <vojtech.vondra@productboard.com>
2023-12-27 22:28:05 +0100

    2023, Day 24, part 1

---
+++ 2023/Day24/Program.cs
@@ -0,0 +1,68 @@
+﻿var input = File.ReadLines("input.txt")
+    .Select(line =>
+    {
+        var parts = line.Split('@');
+        var coordinates = parts[0].Split(',').Select(long.Parse).ToArray();
+        var offsets = parts[1].Split(',').Select(long.Parse).ToArray();
+        return new long[][] { coordinates, offsets };
+    })
+    .ToArray();
+
+
+var counter = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    for (int j = i + 1; j < input.Length; j++)
+    {
+        if (WillIntersect(input[i][0], input[i][1], input[j][0], input[j][1], 200000000000000, 400000000000000))
+        {
+            counter++;
+        }
+    }
+}
+Console.WriteLine(counter);
+
+bool WillIntersect(long[] point1, long[] vector1, long[] point2, long[] vector2, long min, long max)
+{
+    // Calculate the slopes of the two vectors
+    double slope1 = (double)vector1[1] / vector1[0];
+    double slope2 = (double)vector2[1] / vector2[0];
+
+    if (Math.Abs(slope1 - slope2) < 1e-10)
+    {
+        return false;
+    }
+
+    // Calculate the y-intercepts of the two vectors
+    double yIntercept1 = point1[1] - slope1 * point1[0];
+    double yIntercept2 = point2[1] - slope2 * point2[0];
+
+    // Calculate the x-coordinate of the intersection point
+    double intersectionX = (yIntercept2 - yIntercept1) / (slope1 - slope2);
+
+    // Calculate the y-coordinate of the intersection point
+    double intersectionY = slope1 * intersectionX + yIntercept1;
+
+    // Calculate the vectors from the points to the intersection point
+    double[] vectorToIntersection1 = [intersectionX - point1[0], intersectionY - point1[1]];
+    double[] vectorToIntersection2 = [intersectionX - point2[0], intersectionY - point2[1]];
+
+    // Check if the intersection point is in the direction of the vector from the point
+    if (ScalarProduct2d(vector1, vectorToIntersection1) >= 0 && ScalarProduct2d(vector2, vectorToIntersection2) >= 0)
+    {
+
+        // Check if the intersection point is within the range of the ray
+        if (intersectionY >= min && intersectionY <= max && intersectionX >= min && intersectionX <= max)
+        {
+            return true;
+        }
+    }
+
+    // The vectors will not intersect
+    return false;
+}
+
+double ScalarProduct2d(long[] vector1, double[] vector2)
+{
+    return vector1[0] * vector2[0] + vector1[1] * vector2[1];
+}
---
+++ 2023/Day24b/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Z3;
+
+var input = File.ReadLines("input.txt")
+    .Select(line =>
+    {
+        var parts = line.Split('@');
+        var coordinates = parts[0].Split(',').Select(long.Parse).ToArray();
+        var offsets = parts[1].Split(',').Select(long.Parse).ToArray();
+        return new long[][] { coordinates, offsets };
+    })
+    .ToArray();
+
+
+using (Context ctx = new Context())
+{
+    IntExpr x = ctx.MkIntConst("x");
+    IntExpr y = ctx.MkIntConst("y");
+    IntExpr z = ctx.MkIntConst("z");
+    IntExpr vx = ctx.MkIntConst("vx");
+    IntExpr vy = ctx.MkIntConst("vy");
+    IntExpr vz = ctx.MkIntConst("vz");
+
+    Solver s = ctx.MkSolver();
+
+    foreach (var item in input)
+    {
+        var point = item[0];
+        var vector = item[1];
+
+        s.Add(ctx.MkEq(ctx.MkAdd(ctx.MkMul(ctx.MkInt(point[0]), vx), x), ctx.MkInt(vector[0])));
+        s.Add(ctx.MkEq(ctx.MkAdd(ctx.MkMul(ctx.MkInt(point[1]), vy), y), ctx.MkInt(vector[1])));
+        s.Add(ctx.MkEq(ctx.MkAdd(ctx.MkMul(ctx.MkInt(point[2]), vz), z), ctx.MkInt(vector[2])));
+    }
+
+    if (s.Check() == Status.SATISFIABLE)
+    {
+        Console.WriteLine("Found a solution:");
+        Console.WriteLine("Point: ({0}, {1}, {2})", s.Model.Evaluate(x), s.Model.Evaluate(y), s.Model.Evaluate(z));
+        Console.WriteLine("Vector: ({0}, {1}, {2})", s.Model.Evaluate(vx), s.Model.Evaluate(vy), s.Model.Evaluate(vz));
+    }
+    else
+    {
+        Console.WriteLine("No solution found.");
+    }
+}
