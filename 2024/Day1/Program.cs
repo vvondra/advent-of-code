ff82f4f46889e54e2f6e9319f716d29b7c3442aa
Vojtech Vondra <vvondra@microsoft.com>
2024-12-07 09:00:09 +0100

    Kicking off 2024 Day 1

---
+++ 2024/Day1/Program.cs
@@ -0,0 +1,14 @@
+﻿var input = File.ReadLines("input.txt");
+
+var parsedInput = input
+    .Select(l => l.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+var first = parsedInput.Select(l => l[0]).OrderBy(x => x);
+var second = parsedInput.Select(l => l[1]).OrderBy(x => x);
+var result = first.Zip(second, (f, s) => Math.Abs(f - s)).Sum();
+Console.WriteLine(result);
+
+var secondCount = second.GroupBy(s => s).ToDictionary(g => g.Key, g => g.Count());
+var sum = first.Sum(f => f * secondCount.GetValueOrDefault(f, 0));
+
+Console.WriteLine(sum);
