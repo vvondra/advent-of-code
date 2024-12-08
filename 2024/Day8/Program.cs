abd963b1b83d5131a82b686e4132b03db5d2136d
Vojtech Vondra <vvondra@microsoft.com>
2024-12-08 08:58:02 +0100

    refactor

--- 2024/Day8/Program.cs
+++ 2024/Day8/Program.cs
@@ -4,7 +4,7 @@ var maxX = input.Count();
 var maxY = input.First().Length;
 
 var grid = input
-    .SelectMany((line, row) => line.Select((ch, col) => new { Coordinate = (X: row, Y: col), Character = ch }))
+    .SelectMany((line, row) => line.Select((ch, col) => new { Coordinate = new XY(row, col), Character = ch }))
     .Where(item => item.Character != '.')
     .ToDictionary(item => item.Coordinate, item => item.Character);
 
@@ -15,28 +15,14 @@ var pairs = grid.GroupBy(item => item.Value)
             .SelectMany(g => group.Where(gg => gg.Key != g.Key).Select(gg => (gg.Key, g.Key)).ToList())
             .ToList()
     );
-    
-
-static (int X, int Y) Subtract((int X, int Y) a, (int X, int Y) b)
-{
-    return (a.X - b.X, a.Y - b.Y);
-}
-
-static (int X, int Y) Add((int X, int Y) a, (int X, int Y) b)
-{
-    return (a.X + b.X, a.Y + b.Y);
-}
-
 
 var result = pairs
     .Values
     .SelectMany(p => p)
-    .Select(p => {
-        var vector = Subtract(p.Item1, p.Item2);
-        return Add(p.Item1, vector);
-    })
+    .Select(p => p.Item1 + (p.Item1 - p.Item2))
     .Where(xy => xy.X >= 0 && xy.X < maxX && xy.Y >= 0 && xy.Y < maxY)
-    .Distinct();
+    .Distinct()
+    .ToHashSet();
 
 Console.WriteLine(result.Count());
 
@@ -45,13 +31,13 @@ for (var x = 0; x < maxX; x++)
 {
     for (var y = 0; y < maxY; y++)
     {
-        if (result.Contains((x, y)))
+        if (result.Contains(new XY(x, y)))
         {
             Console.Write('#');
         }
-        else if (grid.ContainsKey((x, y)))
+        else if (grid.ContainsKey(new XY(x, y)))
         {
-            Console.Write(grid[(x, y)]);
+            Console.Write(grid[new XY(x, y)]);
         }
         else
         {
@@ -59,4 +45,17 @@ for (var x = 0; x < maxX; x++)
         }
     }
     Console.WriteLine();
+}
+
+record XY(int X, int Y)
+{
+    public static XY operator -(XY a, XY b)
+    {
+        return new XY(a.X - b.X, a.Y - b.Y);
+    }
+
+    public static XY operator +(XY a, XY b)
+    {
+        return new XY(a.X + b.X, a.Y + b.Y);
+    }
 }
