30ba0a7c7b1124ce70c72e61e44828e4584ceacd
Vojtech Vondra <vvondra@microsoft.com>
2024-12-15 16:57:54 +0100

    Day 15 - could be generalized :/

--- 2024/Day15/Program.cs
+++ 2024/Day15/Program.cs
@@ -6,14 +6,12 @@ var grid = parts[0].Split("\n")
             line.Select((ch, col) => new { Coordinate = new XY(row, col), Character = ch })
     )
     .ToDictionary(item => item.Coordinate, item => item.Character);
+var originalGrid = grid.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
 
 var moves = string.Concat(parts[1].Where(c => !char.IsWhiteSpace(c)));
 
 foreach (var move in moves)
 {
-    var control = grid.Count(kvp => kvp.Value == 'O');
-    var gridCopy = grid.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
-
     var start = grid.Keys.First(k => grid[k] == '@');
 
     var dir = move switch
@@ -61,16 +59,6 @@ foreach (var move in moves)
             grid[moved] = grid[prev];
         }
     }
-
-    var control2 = grid.Count(kvp => kvp.Value == 'O');
-    if (control != control2) {
-        Render(gridCopy);
-        Console.WriteLine(dir);
-        Console.WriteLine();
-        Render(grid);
-        throw new Exception("Control mismatch");
-    }
-
 }
 
 var result = grid.Aggregate(0L, (acc, kvp) => acc + kvp.Value switch
@@ -81,17 +69,151 @@ var result = grid.Aggregate(0L, (acc, kvp) => acc + kvp.Value switch
 
 Console.WriteLine(result);
 
-void Render(Dictionary<XY, char> map) {
-    foreach (var row in Enumerable.Range(0, map.Keys.Max(k => k.Y) + 1))
+// Part 2
+var doubleGrid = originalGrid
+    .SelectMany(kvp => new [] {
+        new KeyValuePair<XY, char>(
+            new XY(kvp.Key.Y, kvp.Key.X * 2),
+             kvp.Value switch {
+                '.' => '.',
+                '#' => '#',
+                'O' => '[',
+                '@' => '@',
+                _ => throw new Exception("Invalid character")
+            }
+        ),
+        new KeyValuePair<XY, char>(
+            new XY(kvp.Key.Y, kvp.Key.X * 2 + 1),
+            kvp.Value switch {
+                '.' => '.',
+                '#' => '#',
+                'O' => ']',
+                '@' => '.',
+                _ => throw new Exception("Invalid character")
+            }
+        )
+    })
+    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+foreach (var move in moves)
+{
+    var start = doubleGrid.Keys.First(k => doubleGrid[k] == '@');
+
+    var dir = move switch
     {
-        foreach (var col in Enumerable.Range(0, map.Keys.Max(k => k.X) + 1))
+        '^' => new XY(-1, 0),
+        'v' => new XY(1, 0),
+        '<' => new XY(0, -1),
+        '>' => new XY(0, 1),
+        _ => throw new Exception("Invalid move")
+    };
+
+    if (dir.Y == 0) {
+        var toCheck = start;
+        var toMove = new List<XY>();
+
+        // Horizontal move
+        while (true)
         {
-            Console.Write(map.TryGetValue(new XY(row, col), out var ch) ? ch : ' ');
+            var next = toCheck + dir;
+            if (doubleGrid.TryGetValue(next, out var nextChar))
+            {
+                if (nextChar == '.')
+                {
+                    toMove.Add(toCheck);
+                    toMove.Add(next);
+                    break;
+                }
+                else if (nextChar == '[' || nextChar == ']')
+                {
+                    toMove.Add(toCheck);
+                    toMove.Add(next);
+                    toCheck = next;
+                    next = toCheck + dir;
+                }
+                else if (nextChar == '#')
+                {
+                    toMove.Clear();
+                    break;
+                }
+            }
+            
+            toCheck = next;
         }
-        Console.WriteLine();
+
+        toMove.Reverse();
+        foreach (var moved in toMove) {
+            var prev = moved - dir;
+            if (moved == start) {
+                doubleGrid[moved] = '.';
+            } else {
+                doubleGrid[moved] = doubleGrid[prev];
+            }
+        }
+    } else {
+        // Vertical move
+        var toCheck = new Queue<XY>();
+        var toMove = new List<XY>();
+        toCheck.Enqueue(start);
+        while (toCheck.Count > 0)
+        {
+            var next = toCheck.Dequeue();
+            var nextChar = doubleGrid[next];
+            if (nextChar == '.')
+            {
+                continue;
+            }
+            else if (nextChar == '[')
+            {
+                if (doubleGrid[next + dir] != '#' && doubleGrid[next + dir + new XY(0, 1)] != '#') {
+                    toMove.Add(next);
+                    toMove.Add(next + new XY(0, 1));
+                    toCheck.Enqueue(next + dir);
+                    toCheck.Enqueue(next + dir + new XY(0, 1));
+                } else {
+                    toMove.Clear();
+                    break;
+                }
+            }
+            else if (nextChar == ']')
+            {
+                if (doubleGrid[next + dir] != '#' && doubleGrid[next + dir + new XY(0, -1)] != '#') {
+                    toMove.Add(next);
+                    toMove.Add(next + new XY(0, -1));
+                    toCheck.Enqueue(next + dir);
+                    toCheck.Enqueue(next + dir + new XY(0, -1));
+                } else {
+                    toMove.Clear();
+                    break;
+                }
+            }
+            else if (nextChar == '@')
+            {
+                if (doubleGrid[next + dir] != '#') {
+                    toMove.Add(next);
+                    toCheck.Enqueue(next + dir);
+                } else {
+                    toMove.Clear();
+                    break;
+                }
+            }
+        }
+        toMove = toMove.Distinct().Reverse().ToList();
+        foreach (var moved in toMove) {
+            var next = moved + dir;
+            doubleGrid[next] = doubleGrid[moved];
+            doubleGrid[moved] = '.';
+        }
     }
 }
 
+var result2 = doubleGrid.Aggregate(0L, (acc, kvp) => acc + kvp.Value switch
+{
+    '[' => kvp.Key.X + (100 * kvp.Key.Y),
+    _ => 0
+});
+
+Console.WriteLine(result2);
 
 record XY(int Y, int X)
 {
